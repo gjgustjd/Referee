d2f975846dee560f1a91b2198e164d61f8f53978
허현성 <gjgustjd70@naver.com>
2024-02-12 16:48:35 +0900

    feature:하단 네비게이션 탭 선택에 따라 프래그먼트 전환 추가

--- app/src/main/java/com/example/referee/MainActivity.kt
+++ app/src/main/java/com/example/referee/MainActivity.kt
@@ -2,10 +2,73 @@ package com.example.referee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.referee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+       binding.bottomNavigationView.setOnItemSelectedListener {
+           val action = when (it.itemId) {
+               R.id.menu_ingredients -> {
+                  when (binding.bottomNavigationView.selectedItemId) {
+                       R.id.menu_fridge -> {
+                           FridgeFragmentDirections.actionFridgeFragmentToIngredientsFragment()
+                       }
+
+                       R.id.menu_cook -> {
+                           CookFragmentDirections.actionCookFragmentToIngredientsFragment()
+                       }
+                       else -> null
+                   }
+               }
+
+               R.id.menu_fridge -> {
+                   when (binding.bottomNavigationView.selectedItemId) {
+                       R.id.menu_ingredients -> {
+                           IngredientsFragmentDirections.actionIngredientsFragmentToFridgeFragment()
+                       }
+
+                       R.id.menu_cook -> {
+                           CookFragmentDirections.actionCookFragmentToFridgeFragment()
+                       }
+
+                       else -> null
+                   }
+               }
+
+               R.id.menu_cook -> {
+                   when (binding.bottomNavigationView.selectedItemId) {
+                       R.id.menu_ingredients -> {
+                           IngredientsFragmentDirections.actionIngredientsFragmentToCookFragment()
+                       }
+
+                       R.id.menu_fridge -> {
+                           FridgeFragmentDirections.actionFridgeFragmentToCookFragment()
+                       }
+
+                       else -> null
+                   }
+               }
+
+               else -> null
+           }
+
+           action?.let {
+               Navigation.findNavController(binding.navHostFragment).navigate(action)
+               true
+           } ?: run {
+               false
+           }
+       }
     }
 }
