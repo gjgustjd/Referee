6d29c1cbb5594f40dfb6744e27a719734a4ba322
허현성 <gjgustjd70@naver.com>
2024-02-18 03:36:37 +0900

    feat:이미지 클릭 시 카메라 및 갤러리에서 이미지 선택하여 저장하는 기능 구현

--- app/src/main/java/com/example/referee/ingredientadd/IngredientAddActivity.kt
+++ app/src/main/java/com/example/referee/ingredientadd/IngredientAddActivity.kt
@@ -1,15 +1,24 @@
 package com.example.referee.ingredientadd
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Rect
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ArrayAdapter
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.referee.R
 import com.example.referee.databinding.ActivityAddIngredientBinding
+import java.io.File
 
 class IngredientAddActivity:AppCompatActivity() {
     lateinit var binding: ActivityAddIngredientBinding
@@ -23,6 +32,91 @@ class IngredientAddActivity:AppCompatActivity() {
     private fun initViews() {
         initUnitRecyclerView()
         initExpirationSpinner()
+        initPhoto()
+    }
+
+    private fun initPhoto() {
+        val cameraActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+           if(result.resultCode == RESULT_OK) {
+               val imageBitmap = result.data?.extras?.get("data") as Bitmap
+               binding.ivPhoto.setImageBitmap(imageBitmap)
+           }
+        }
+
+        val galleryActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == RESULT_OK) {
+                val imageUri = result.data?.data
+                imageUri?.let {
+                    binding.ivPhoto.setImageURI(it)
+                }
+            }
+        }
+        binding.ivPhoto.setOnClickListener {
+            val dialog = AlertDialog.Builder(this).apply {
+                setTitle(R.string.ingredient_add_photo_desc)
+                setItems(
+                    arrayOf(
+                        getString(R.string.ingredient_add_photo_camera),
+                        getString(R.string.ingredient_add_photo_gallery)
+                    )
+                ) { _, which ->
+                    when(which) {
+                        0 -> {
+                            val cameraPermissionCheck = ContextCompat.checkSelfPermission(
+                                this@IngredientAddActivity,
+                                android.Manifest.permission.CAMERA
+                            )
+
+                            if (cameraPermissionCheck != PackageManager.PERMISSION_GRANTED) { // 권한이 없는 경우
+                                ActivityCompat.requestPermissions(
+                                    this@IngredientAddActivity,
+                                    arrayOf(android.Manifest.permission.CAMERA),
+                                    1000
+                                )
+                            } else { //권한이 있는 경우
+                                Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                                    takePictureIntent.resolveActivity(packageManager)?.also {
+                                        cameraActivityResult.launch(takePictureIntent)
+                                    }
+                                }
+                            }
+                        }
+                        1 -> {
+                            val galleryPermission = android.Manifest.permission.READ_MEDIA_IMAGES
+                            val galleryPermissionCheck = ContextCompat.checkSelfPermission(
+                                this@IngredientAddActivity,
+                                galleryPermission
+                            ) != PackageManager.PERMISSION_GRANTED
+
+                            if (galleryPermissionCheck) { // 권한이 없는 경우
+                                ActivityCompat.requestPermissions(
+                                    this@IngredientAddActivity,
+                                    arrayOf(galleryPermission),
+                                    1001
+                                )
+                            } else { //권한이 있는 경우
+                               Intent().apply {
+                                   type = "image/*"
+                                   action = Intent.ACTION_GET_CONTENT
+                                   addCategory(Intent.CATEGORY_OPENABLE)
+                               }.also { takePictureIntent ->
+                                   takePictureIntent.resolveActivity(packageManager)?.also {
+                                       galleryActivityResult.launch(takePictureIntent)
+                                   }
+                                }
+                            }
+                        }
+                    }
+                }
+                setPositiveButton(R.string.confirm) {dialog,id ->
+
+                }
+                setNegativeButton(R.string.cancel) { dialog,id ->
+
+                }
+            }
+            dialog.create().show()
+        }
     }
 
     private fun initExpirationSpinner() {
