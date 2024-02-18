0658d14f2ed848f6b00bb86ad721da7aacd0d3dd
허현성 <gjgustjd70@naver.com>
2024-02-18 23:42:51 +0900

    refactor:viewModel 로직 정리

--- app/src/main/java/com/example/referee/ingredientadd/IngredientAddViewModel.kt
+++ app/src/main/java/com/example/referee/ingredientadd/IngredientAddViewModel.kt
@@ -2,16 +2,15 @@ package com.example.referee.ingredientadd
 
 import android.graphics.Bitmap
 import androidx.lifecycle.viewModelScope
-import com.example.referee.common.base.BaseViewModel
 import com.example.referee.common.EventWrapper
+import com.example.referee.common.base.BaseViewModel
 import com.example.referee.ingredientadd.model.IngredientEntity
 import com.example.referee.ingredientadd.model.IngredientExpirationUnit
 import com.example.referee.ingredientadd.model.IngredientRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
-import java.lang.Exception
 
 class IngredientAddViewModel : BaseViewModel<IngredientAddEvent>() {
 
@@ -22,25 +21,25 @@ class IngredientAddViewModel : BaseViewModel<IngredientAddEvent>() {
         expiration: IngredientExpirationUnit
     ) {
         val byteArrayOutputStream = ByteArrayOutputStream()
-        photoPath.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        photoPath.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream)
         val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
         val item = IngredientEntity(name, byteArray, unit, expiration.days)
 
         viewModelScope.launch(Dispatchers.IO) {
-            viewModelScope.async(Dispatchers.IO) {
-                try {
-                    val result = IngredientRepository.insertIngredient(item) > 0
-
-                    _event.postValue(
-                        if (result) {
-                            EventWrapper(IngredientAddEvent.InsertSuccess)
-                        } else {
-                            EventWrapper(IngredientAddEvent.InsertFailed)
-                        }
-                    )
-                } catch (e: Exception) {
-                    _event.postValue(EventWrapper(IngredientAddEvent.InsertFailed))
+            try {
+                val result = withContext(Dispatchers.IO) {
+                    IngredientRepository.insertIngredient(item) > 0
                 }
+
+                _event.postValue(
+                    if (result) {
+                        EventWrapper(IngredientAddEvent.InsertSuccess)
+                    } else {
+                        EventWrapper(IngredientAddEvent.InsertFailed)
+                    }
+                )
+            } catch (e: Exception) {
+                _event.postValue(EventWrapper(IngredientAddEvent.InsertFailed))
             }
         }
     }
--- app/src/main/java/com/example/referee/ingredients/IngredientsFragmentViewModel.kt
+++ app/src/main/java/com/example/referee/ingredients/IngredientsFragmentViewModel.kt
@@ -1,14 +1,12 @@
 package com.example.referee.ingredients
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.example.referee.common.EventWrapper
 import com.example.referee.common.base.BaseViewModel
 import com.example.referee.ingredientadd.model.IngredientRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import java.lang.Exception
+import kotlinx.coroutines.withContext
 
 class IngredientsFragmentViewModel :
     BaseViewModel<IngredientsEvent>() {
@@ -16,10 +14,9 @@ class IngredientsFragmentViewModel :
     fun getIngredientsList() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = async(Dispatchers.IO) {
+                val result = withContext(Dispatchers.IO) {
                     IngredientRepository.getIngredientsList()
-                }.await()
-                Log.i("ResultTest", result.toString())
+                }
                 _event.postValue(EventWrapper(IngredientsEvent.GetIngredients.Success(result)))
             } catch (e: Exception) {
                 _event.postValue(EventWrapper(IngredientsEvent.GetIngredients.Failed))
