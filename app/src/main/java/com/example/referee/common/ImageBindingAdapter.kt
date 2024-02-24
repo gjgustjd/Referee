885844b29d41361ad6432eca9de651135f7ca58d
허현성 <gjgustjd70@naver.com>
2024-02-25 00:54:52 +0900

    fix:캐싱 제거

--- app/src/main/java/com/example/referee/common/ImageBindingAdapter.kt
+++ app/src/main/java/com/example/referee/common/ImageBindingAdapter.kt
@@ -6,7 +6,6 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.referee.ingredientadd.model.IngredientCategoryType
 
 object ImageBindingAdapter {
@@ -19,7 +18,7 @@ object ImageBindingAdapter {
                 .with(view.context)
                 .load(bitmap)
                 .thumbnail(0.1f)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .skipMemoryCache(true)
                 .into(view)
         }
     }
