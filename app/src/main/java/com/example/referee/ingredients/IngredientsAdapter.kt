c4da436f08bedddea63099f9f2447230e0c8e494
허현성 <gjgustjd70@naver.com>
2024-02-25 01:00:05 +0900

    feat:뷰 재활용 시 이전 이미지 언로드

--- app/src/main/java/com/example/referee/ingredients/IngredientsAdapter.kt
+++ app/src/main/java/com/example/referee/ingredients/IngredientsAdapter.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.referee.databinding.ItemIngredientBinding
 import com.example.referee.ingredientadd.model.IngredientEntity
 
@@ -51,6 +52,8 @@ class IngredientsAdapter(
 
             item.imageBitmap ?: run {
                 item.photoName?.let {
+                    Glide.with(binding.ivThumbnail.context)
+                        .clear(binding.ivThumbnail)
                     bindThumbFun(it, position)
                 }
             }
