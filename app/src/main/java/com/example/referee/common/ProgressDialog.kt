c16f5a5f14d8b19dc381e57e4edc0285307d0585
허현성 <gjgustjd70@naver.com>
2024-02-25 19:31:31 +0900

    refactor:layoutResourceId 및 binding Base로 분리

--- app/src/main/java/com/example/referee/common/ProgressDialog.kt
+++ app/src/main/java/com/example/referee/common/ProgressDialog.kt
@@ -3,28 +3,19 @@ package com.example.referee.common
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatDialog
-import androidx.databinding.DataBindingUtil
 import com.example.referee.R
 import com.example.referee.common.base.BaseAppCompatDialogFragment
 import com.example.referee.databinding.ViewLoadingProgressbarBinding
 
-class ProgressDialog : BaseAppCompatDialogFragment() {
+class ProgressDialog :
+    BaseAppCompatDialogFragment<ViewLoadingProgressbarBinding>(R.layout.view_loading_progressbar) {
 
     override fun onCreateDialog(): AppCompatDialog {
-        val binding: ViewLoadingProgressbarBinding = DataBindingUtil.inflate(
-            layoutInflater,
-            R.layout.view_loading_progressbar,
-            null,
-            false
-        )
-
-        val dialog = AppCompatDialog(requireContext()).apply {
+        return AppCompatDialog(requireContext()).apply {
             setContentView(binding.root)
             setCancelable(false)
             setCanceledOnTouchOutside(false)
             window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
         }
-
-        return dialog
     }
 }
--- app/src/main/java/com/example/referee/common/base/BaseAppCompatDialogFragment.kt
+++ app/src/main/java/com/example/referee/common/base/BaseAppCompatDialogFragment.kt
@@ -5,9 +5,21 @@ import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatDialog
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
-abstract class BaseAppCompatDialogFragment : AppCompatDialogFragment() {
+abstract class BaseAppCompatDialogFragment<B : ViewDataBinding>(private val layoutResourceId: Int) :
+    AppCompatDialogFragment() {
+
     var onBackPressed: (() -> Unit)? = null
+    protected val binding: B by lazy {
+        DataBindingUtil.inflate(
+            layoutInflater,
+            layoutResourceId,
+            null,
+            false
+        )
+    }
 
     /* Dialog가 show를 통해 보여지기 전 */
     override fun onStart() {
