afd0eb7293976383bd1758d0c5f362b52ededf7e
허현성 <gjgustjd70@naver.com>
2024-02-25 23:30:51 +0900

    refactor:Deferred 사용하여 식재료 저장 코드 간소화

--- app/src/main/java/com/example/referee/ingredientadd/IngredientAddViewModel.kt
+++ app/src/main/java/com/example/referee/ingredientadd/IngredientAddViewModel.kt
@@ -9,8 +9,10 @@ import com.example.referee.ingredientadd.model.IngredientCategoryType
 import com.example.referee.ingredientadd.model.IngredientEntity
 import com.example.referee.ingredientadd.model.IngredientExpirationUnit
 import com.example.referee.ingredientadd.model.IngredientRepository
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -18,8 +20,7 @@ import java.io.FileOutputStream
 
 class IngredientAddViewModel : BaseViewModel<IngredientAddEvent>() {
 
-    private var savedImageName: String? = null
-    private var saveImageJob: Job? = null
+    private var savedImageName: Deferred<String?>? = null
 
     fun insertIngredient(
         name: String,
@@ -28,46 +29,29 @@ class IngredientAddViewModel : BaseViewModel<IngredientAddEvent>() {
         category:IngredientCategoryType
     ): Job {
         return viewModelScope.launch(Dispatchers.IO) {
-            var photoName = savedImageName
-            photoName?.let {
-                insertIngredientIntoDB(name, unit, it.ifEmpty { null }, expiration, category)
-            } ?: saveImageJob?.invokeOnCompletion {
-                viewModelScope.launch(Dispatchers.IO) {
-                    photoName = savedImageName?.ifEmpty { null }
-                    insertIngredientIntoDB(name, unit, photoName, expiration, category)
-                }
-            }
-        }
-    }
-
-    private suspend fun insertIngredientIntoDB(
-        name: String,
-        unit: String,
-        imageName: String?,
-        expiration: IngredientExpirationUnit,
-        category: IngredientCategoryType
-    ) {
-        val item = IngredientEntity(
-            name,
-            imageName,
-            unit,
-            expiration.days,
-            category.ordinal
-        )
-        try {
-            val result = withContext(Dispatchers.IO) {
-                IngredientRepository.insertIngredient(item) > 0
-            }
-
-            _event.postValue(
-                if (result) {
-                    EventWrapper(IngredientAddEvent.InsertSuccess)
-                } else {
-                    EventWrapper(IngredientAddEvent.InsertFailed)
-                }
+            val photoName = savedImageName?.await()
+            val item = IngredientEntity(
+                name,
+                photoName,
+                unit,
+                expiration.days,
+                category.ordinal
             )
-        } catch (e: Exception) {
-            _event.postValue(EventWrapper(IngredientAddEvent.InsertFailed))
+            try {
+                val result = withContext(Dispatchers.IO) {
+                    IngredientRepository.insertIngredient(item) > 0
+                }
+
+                _event.postValue(
+                    if (result) {
+                        EventWrapper(IngredientAddEvent.InsertSuccess)
+                    } else {
+                        EventWrapper(IngredientAddEvent.InsertFailed)
+                    }
+                )
+            } catch (e: Exception) {
+                _event.postValue(EventWrapper(IngredientAddEvent.InsertFailed))
+            }
         }
     }
 
@@ -84,23 +68,25 @@ class IngredientAddViewModel : BaseViewModel<IngredientAddEvent>() {
     }
 
      fun saveImage(bitmap: Bitmap?) {
-        saveImageJob = viewModelScope.launch(Dispatchers.IO) {
-            savedImageName = bitmap?.let {
-                try {
-                    val storage = RefereeApplication.instance().cacheDir
-                    val fileName = "ingredient_" + System.currentTimeMillis().toString()
-                    val tempFile = File(storage, fileName)
+         viewModelScope.launch(Dispatchers.IO) {
+             savedImageName = viewModelScope.async(Dispatchers.IO) {
+                 bitmap?.let {
+                     try {
+                         val storage = RefereeApplication.instance().cacheDir
+                         val fileName = "ingredient_" + System.currentTimeMillis().toString()
+                         val tempFile = File(storage, fileName)
 
-                    tempFile.createNewFile()
-                    val outStream = FileOutputStream(tempFile)
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)
-                    outStream.close()
+                         tempFile.createNewFile()
+                         val outStream = FileOutputStream(tempFile)
+                         bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)
+                         outStream.close()
 
-                    fileName
-                } catch (e: java.lang.Exception) {
-                    ""
-                }
-            }
-        }
-    }
+                         fileName
+                     } catch (e: java.lang.Exception) {
+                         null
+                     }
+                 }
+             }
+         }
+     }
 }
