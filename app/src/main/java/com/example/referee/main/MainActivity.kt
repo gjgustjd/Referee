8372bb19bcbf1c3f2708d23b0d95234be427dcd1
허현성 <gjgustjd70@naver.com>
2024-03-03 23:09:57 +0900

    feat:ViewPager2 적용

--- app/src/main/java/com/example/referee/main/MainActivity.kt
+++ app/src/main/java/com/example/referee/main/MainActivity.kt
@@ -1,88 +1,52 @@
 package com.example.referee.main
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.fragment.app.commit
+import androidx.viewpager2.widget.ViewPager2
 import com.example.referee.R
 import com.example.referee.common.base.BaseActivity
 import com.example.referee.databinding.ActivityMainBinding
-import com.example.referee.ingredients.IngredientsFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private var fridgeFragment: FridgeFragment? = null
-    private var cookFragment: CookFragment? = null
-    private var ingredientsFragment: IngredientsFragment? = null
-
     override fun initViews() {
-        supportFragmentManager.commit {
-            fridgeFragment?.let { fridge ->
-                hideOtherFragment(fridgeFragment)
-                show(fridge)
-            } ?: run {
-                fridgeFragment = FridgeFragment()
-                fridgeFragment?.let {
-                    add(R.id.fragmentMain, it)
+        with(binding.vpMain) {
+            adapter = MainPagerAdapter(this@MainActivity)
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNavigationView.menu.getItem(position).isChecked = true
                 }
-            }
-            title = getString(R.string.navigation_menu_fridger)
-       }
+            })
+        }
     }
 
     override fun initListeners() {
         super.initListeners()
 
         binding.bottomNavigationView.setOnItemSelectedListener {
-            val fm = supportFragmentManager.beginTransaction()
-            var menuTitle: String? = null
-            val currentFragment = when (it.itemId) {
-                R.id.menu_ingredients -> {
-                    menuTitle = getString(R.string.navigation_menu_ingredient)
-                    ingredientsFragment ?: run {
-                        ingredientsFragment = IngredientsFragment()
-                        ingredientsFragment?.apply {
-                            fm.add(R.id.fragmentMain,this)
-                        }
+            val menuTitle: String
+            binding.vpMain.currentItem =
+                when (it.itemId) {
+                    R.id.menu_fridge -> {
+                        menuTitle = getString(R.string.navigation_menu_fridger)
+                        0
                     }
-                }
 
-                R.id.menu_fridge -> {
-                    menuTitle = getString(R.string.navigation_menu_fridger)
-                    fridgeFragment ?: run {
-                        fridgeFragment = FridgeFragment()
-                        fridgeFragment?.apply {
-                            fm.add(R.id.fragmentMain,this)
-                        }
+                    R.id.menu_cook -> {
+                        menuTitle = getString(R.string.navigation_menu_cook)
+                        1
                     }
-                }
 
-                R.id.menu_cook -> {
-                    menuTitle = getString(R.string.navigation_menu_cook)
-                    cookFragment ?: run {
-                        cookFragment = CookFragment()
-                        cookFragment?.apply {
-                            fm.add(R.id.fragmentMain,this)
-                        }
+                    R.id.menu_ingredients -> {
+                        menuTitle = getString(R.string.navigation_menu_ingredient)
+                        2
                     }
-                }
 
-                else -> return@setOnItemSelectedListener false
-            }
-
-            fm.apply {
-                currentFragment?.let {fragment->
-                    hideOtherFragment(fragment)
-                    show(fragment)
+                    else -> return@setOnItemSelectedListener false
                 }
-            }.commit()
             title = menuTitle
 
             return@setOnItemSelectedListener true
-       }
-    }
-
-    private fun FragmentTransaction.hideOtherFragment(fragment: Fragment?) {
-        val fragments = arrayOf(fridgeFragment,cookFragment,ingredientsFragment)
-        fragments.filter { it != fragment }.onEach { it?.let(::hide) }
+        }
     }
 }
