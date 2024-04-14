9ceed9724f2307140a93620fc1a0e989dd01d158
허현성 <gjgustjd70@naver.com>
2024-04-14 17:30:47 +0900

    feat:식재료 검색 화면 추가

---
+++ app/src/main/java/com/example/referee/fridge/SearchIngredientsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.referee.fridge
+
+import com.example.referee.R
+import com.example.referee.common.base.BaseActivity
+import com.example.referee.databinding.ActivitySearchIngredientsBinding
+
+class SearchIngredientsActivity :
+    BaseActivity<ActivitySearchIngredientsBinding>(R.layout.activity_search_ingredients) {
+
+    override fun initViews() {
+    }
+}
--- app/src/main/java/com/example/referee/main/FridgeFragment.kt
+++ app/src/main/java/com/example/referee/main/FridgeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.referee.main
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.referee.R
 import com.example.referee.common.base.BaseFragment
 import com.example.referee.databinding.FragmentFridgeBinding
+import com.example.referee.fridge.SearchIngredientsActivity
 
 class FridgeFragment : BaseFragment<FragmentFridgeBinding>(R.layout.fragment_fridge) {
 
@@ -17,6 +19,11 @@ class FridgeFragment : BaseFragment<FragmentFridgeBinding>(R.layout.fragment_fri
     }
 
     override fun initViews() {
+        binding.fabAddIngredient.setOnClickListener {
+            activity?.let {
+                startActivity(Intent(it, SearchIngredientsActivity::class.java))
+            }
+        }
     }
     override fun initListeners() = Unit
 }
