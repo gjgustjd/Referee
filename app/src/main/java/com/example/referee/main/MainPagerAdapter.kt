bef60cccc4b31c59f0e6ef04b6677784ca61dc48
허현성 <gjgustjd70@naver.com>
2024-05-19 17:31:00 +0900

    refactor:기존 ContainerFragment 제거

--- app/src/main/java/com/example/referee/main/MainPagerAdapter.kt
+++ app/src/main/java/com/example/referee/main/MainPagerAdapter.kt
@@ -3,22 +3,21 @@ package com.example.referee.main
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.referee.common.ContainerFragment
 import com.example.referee.common.base.BaseFragment
 import com.example.referee.fridge.FridgeFragment
 import com.example.referee.ingredients.IngredientsFragment
 import com.example.referee.recipe.RecipeFragment
 
 class MainPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
-    private val fragments: List<ContainerFragment> = listOf(
-        ContainerFragment().apply { childFragment = FridgeFragment() },
-        ContainerFragment().apply { childFragment = RecipeFragment() },
-        ContainerFragment().apply { childFragment = IngredientsFragment() }
+    private val fragments: List<BaseFragment<*>> = listOf(
+        FridgeFragment.newInstance(),
+        RecipeFragment(),
+        IngredientsFragment()
     )
 
     override fun getItemCount(): Int = fragments.size
 
     override fun createFragment(position: Int): Fragment = fragments[position]
 
-    fun getBaseFragment(position: Int) = fragments[position].childFragment as BaseFragment<*>
+    fun getBaseFragment(position: Int) = fragments[position]
 }
