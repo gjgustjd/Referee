751bd2ba6437eda87f0e52e5e688303f304bb37c
허현성 <gjgustjd70@naver.com>
2024-10-01 23:39:02 +0900

    refactor(dao):냉장고 식재료 삽입 시 레시피 캐시 말소하도록 변경

--- app/src/main/java/com/example/referee/fridge/model/FridgeDAO.kt
+++ app/src/main/java/com/example/referee/fridge/model/FridgeDAO.kt
@@ -2,6 +2,7 @@ package com.example.referee.fridge.model
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import com.example.referee.common.model.BaseDAO
 import kotlinx.coroutines.flow.Flow
 
@@ -10,4 +11,13 @@ interface FridgeDAO:BaseDAO<FridgeIngredientEntity> {
 
     @Query("SELECT * FROM fridge")
     fun getFridgeIngredients(): Flow<List<FridgeIngredientEntity>>
+
+    @Transaction
+    fun insertIngredientAndClearRecipeCache(ingredient:FridgeIngredientEntity) {
+        insert(ingredient)
+        clearRecipeCache()
+    }
+
+    @Query("DELETE FROM recipes_cache")
+    fun clearRecipeCache()
 }
--- app/src/main/java/com/example/referee/fridge/model/FridgeRepository.kt
+++ app/src/main/java/com/example/referee/fridge/model/FridgeRepository.kt
@@ -2,13 +2,9 @@ package com.example.referee.fridge.model
 
 import com.example.referee.common.applicationScope
 import com.example.referee.common.base.BaseLocalRepository
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 
 object FridgeRepository: BaseLocalRepository() {
 
@@ -22,7 +18,7 @@ object FridgeRepository: BaseLocalRepository() {
 
     @Synchronized
     fun insertIngredientToFridge(entity: FridgeIngredientEntity) =
-        db.fridgeDAO().insert(entity)
+        db.fridgeDAO().insertIngredientAndClearRecipeCache(entity)
 
     private fun getFridgeItems() = db.fridgeDAO().getFridgeIngredients()
 }
